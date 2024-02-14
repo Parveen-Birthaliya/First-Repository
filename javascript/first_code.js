6c2796d7530ac0577016adccc046ddf899ee280d
Parveen-Birthaliya <159903740+Parveen-Birthaliya@users.noreply.github.com>
2024-02-14 06:02:07 +0000

    defined varible and its type and assining commit by me

--- javascript/first_code.js
+++ javascript/first_code.js
@@ -1,1 +1,30 @@
 console.log("Hello World !")
+
+// now defineing variable and its type
+
+//const => this type defined variable is not changeable once it assign
+const name ="Parveen"
+
+//var => this type defined varible is changeable
+var n=1 
+
+// let => this type defined varible is changeable and similer to var by this scope restriction can be avoid  
+let num=5.6 
+
+//data types
+
+//num => 2 to power of 53 
+let m=5
+
+//bigint => use to calculate big num that is not in the scope of num like in online stock market 
+
+// string => it can be define by just putting word or characters in ""
+const Name ="Parveen"
+
+//boolean => True or false
+let isBoy=true
+
+//undefined => not assinging value 
+let varible;
+
+//NULL => its mean there is noting the variable contain but in undefined there must be some garbage value
