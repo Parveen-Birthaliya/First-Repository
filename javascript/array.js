9293590dc9daf44ab06d032f4b7f36802e5ed876
Parveen-Birthaliya <159903740+Parveen-Birthaliya@users.noreply.github.com>
2024-02-17 08:15:49 +0000

    array

---
+++ javascript/array.js
@@ -0,0 +1,13 @@
+const arr = [1,3,5,7]
+const Arr= [2,4,6,8]
+
+// arr.push(Arr)
+// console.log(arr)
+
+// const Allarr=arr.concat(Arr)
+// console.log(Allarr)
+
+// spread => [...arr, ...Arr]
+// .flat(infinity)=> spreading all array in array by infinty depth
+
+console.log(Array.from("Parveen"))
