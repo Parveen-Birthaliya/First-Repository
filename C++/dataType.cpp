b124be2e4cc0c3a5e4e525f218cfa4c47bc58213
Parveen-Birthaliya <159903740+Parveen-Birthaliya@users.noreply.github.com>
2024-03-14 02:04:00 +0000

    C++

---
+++ C++/dataType.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    cout<<a<<endl;
+
+    char b = 'c';
+    cout<<b<<endl;
+
+    double c= 187;
+    cout<<c<<endl;
+
+    bool d = true ;
+    cout<<d<<endl;
+
+    float e=3.124;
+    cout<<e<<endl;
+
+    int size =sizeof(c);
+    cout<<"size "<<size<<endl;
+
+}
